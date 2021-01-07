7a6f99087458dfe44ffb3e17b1bc7e73dc7a874d
sylkicks <74621222+sylkicks@users.noreply.github.com>
2021-01-07 13:03:39 -0700

    Added hamburger menu and adjusted for layout/design issues.

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,9 @@
 console.log("You've found the work of Sean Williams! Stay awhile, and listen.")
+function menuToggle() {
+    var x = document.getElementById(myNavtoggle);
+    if (x.className === 'navtoggle') {
+        x.className += ' responsive';
+    } else {
+        x.className = 'navtoggle';
+    }  
+}
